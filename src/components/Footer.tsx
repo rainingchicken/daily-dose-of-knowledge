217e7e679f27e792168a860b09d32e55ec2776e7
rainingchicken <154983686+rainingchicken@users.noreply.github.com>
2025-06-02 19:40:58 -0700

    add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="text-center">
+      <p>
+        Special Thanks to
+        <a href="https://github.com/Anas099X/OpenSAT?tab=readme-ov-file">
+          {" "}
+          Anas099X{" "}
+        </a>
+        for the public API
+      </p>
+      <p>
+        My GitHub <a href="Anas099X">rainingchicken</a>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
