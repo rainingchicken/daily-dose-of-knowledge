fce96861a99ccc073e17420710ead8b881c4182f
rainingchicken <154983686+rainingchicken@users.noreply.github.com>
2025-06-03 20:14:50 -0700

    fix hydration error maybe

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,8 +25,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <body
+        suppressHydrationWarning
         className={`${geistSans.variable} ${geistMono.variable} antialiased container mx-auto p-4 md:p-8 justify-center`}
       >
         <Navbar />
