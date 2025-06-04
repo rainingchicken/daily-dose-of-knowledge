002df9127b4a6861496c46ebb219ef425d80b18a
rainingchicken <154983686+rainingchicken@users.noreply.github.com>
2025-06-03 20:32:09 -0700

    put loading skeleton in components and use it for both pages

--- src/app/(pages)/english/page.tsx
+++ src/app/(pages)/english/page.tsx
@@ -1,7 +1,7 @@
 import React, { Suspense } from "react";
-import LoadingSkeleton from "./LoadingSkeleton";
 import { getSATQuestions } from "@/utils/api";
 import QuestionCard from "@/components/QuestionCard";
+import QuestionCardSkeleton from "@/components/QuestionCardLoadingSkeleton";
 
 const page = () => {
   return (
@@ -9,7 +9,7 @@ const page = () => {
       <h1 className="text-center text-warning text-2xl font-bold m-5">
         Daily Dose of English
       </h1>
-      <Suspense fallback={<LoadingSkeleton />}>
+      <Suspense fallback={<QuestionCardSkeleton />}>
         <MathQuestion />
       </Suspense>
     </>
