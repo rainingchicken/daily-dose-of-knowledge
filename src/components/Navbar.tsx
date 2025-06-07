db3eea01f65a1c186c13e173292439935f0ca3c8
rainingchicken <154983686+rainingchicken@users.noreply.github.com>
2025-06-06 23:34:10 -0700

    make navbar scalable and fix lint error to use Link to nav to home instead of a

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,22 @@
 "use client";
 
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
+
+const navLinks = [
+  { href: "/math", text: "Math Problem" },
+  { href: "/english", text: "English Problem" },
+];
 
 const Navbar = () => {
   const pathname = usePathname();
   return (
     <div className="navbar bg-base-100 shadow-sm">
       <div className="flex-1">
-        <a href="/" className="btn btn-ghost text-xl">
+        <Link href="/" className="btn btn-ghost text-xl">
           DDK Home
-        </a>
+        </Link>
       </div>
       <div className="dropdown dropdown-hover dropdown-end">
         <div tabIndex={0} role="button" className="btn m-1">
@@ -32,26 +38,21 @@ const Navbar = () => {
           tabIndex={0}
           className="menu dropdown-content rounded-box z-1 w-52 p-2 shadow-sm"
         >
-          <li>
-            <a
-              href="/math"
-              className={`btn btn-block ${
-                pathname === "/math" ? "btn-info" : ""
-              }`}
-            >
-              Math Problem
-            </a>
-          </li>
-          <li>
-            <a
-              href="/english"
-              className={`btn btn-block ${
-                pathname === "/english" ? "btn-info" : ""
-              }`}
-            >
-              English Problem
-            </a>
-          </li>
+          {navLinks &&
+            navLinks.map((link) => {
+              return (
+                <li key={link.href}>
+                  <a
+                    href={link.href}
+                    className={`btn btn-block ${
+                      pathname === link.href ? "btn-info" : ""
+                    }`}
+                  >
+                    {link.text}
+                  </a>
+                </li>
+              );
+            })}
         </ul>
       </div>
     </div>
