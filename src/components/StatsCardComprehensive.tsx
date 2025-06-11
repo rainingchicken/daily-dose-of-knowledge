04743b2eaf02047927a328ea432299895b089270
rainingchicken <154983686+rainingchicken@users.noreply.github.com>
2025-06-10 23:07:13 -0700

    stats best and worst categories

--- src/components/StatsCardComprehensive.tsx
+++ src/components/StatsCardComprehensive.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import Table from "./Table";
 import { useLocalStorage } from "@/utils/localStorage";
 import { getBestAndWorst } from "@/utils/stats";
+import { Crown, Frown } from "lucide-react";
 
 interface StatsCardComprehensiveProp {
   subject: ISubject;
@@ -15,10 +16,34 @@ const StatsCardComprehensive = ({ subject }: StatsCardComprehensiveProp) => {
   const bestWorst = getBestAndWorst(stats, subject);
   return (
     <>
-      <h2>Best Category</h2>
-      <h3>{bestWorst.categoryResult.best}</h3>
-      <h2>Worst Category</h2>
-      <h3>{bestWorst.categoryResult.worst}</h3>
+      <div className="stats bg-background/20 flex justify-between text-center mb-5">
+        <div className="stat">
+          <div className="stat-title">
+            <h2 className="flex justify-center gap-2 pb-2">
+              Best Category <Crown />
+            </h2>
+          </div>
+          <div className="stat-value whitespace-normal ">
+            <h3 className="text-2xl text-success">
+              {bestWorst.categoryResult.best}
+            </h3>
+          </div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-title">
+            <h2 className="flex justify-center gap-2 pb-2">
+              Worst Category <Frown />
+            </h2>
+          </div>
+          <div className="stat-value whitespace-normal ">
+            <h3 className="text-2xl text-error">
+              {bestWorst.categoryResult.worst}
+            </h3>
+          </div>
+        </div>
+      </div>
+
       <Table stats={stats} subject={subject} />
     </>
   );
